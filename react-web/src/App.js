83f13ef3a20bddb3bbaf61f821bf95b5f1ffc1fb
netzebandito <blake.netzeband@gmail.com>
2018-07-24 15:36:20 -0600

    added landing page. added background to welcome text. added postcss

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import style from './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class App extends Component {
+	render() {
+		return (
+			<section>
+				<div>
+					<h1>BLEARN</h1>
+					<p>A Multidimensional learning app. Sounds good to me.</p>
+				</div>
+
+				<div>
+					<input type="text" />
+				</div>
+			</section>
+		);
+	}
 }
-
-export default App;
